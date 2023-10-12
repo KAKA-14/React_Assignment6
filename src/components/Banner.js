3298a0ed81bd5b21d396f4f4cf57f364e84133d1
KAKA-14 <kanishakkhanna@gmail.com>
2023-10-13 04:14:15 +0530

    Banner

---
+++ src/components/Banner.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import { Row, Container, Col } from "react-bootstrap";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import headerImg from '../assets/img/header-img.svg';
+const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);//to keep track of which word to display
+    const [isDeleting, setIsDeleting] = useState(false);//to check if deleting is going on or not
+    const [text, setText] = useState('');//which will be displayed
+    const [delta, setDelta] = useState(200 - Math.random() * 100);//time for which a letter will be displayed
+    const toRotate = [ " Kanishak Khanna","a Full Stack Developer", "a MERN Stack Developer", "a Machine Learning Engineer"];
+    const period = 1000;//time for which a word will be displayed
+    useEffect(() => {
+        let ticker = setInterval(() => { tick(); }, delta);
+        return () => clearInterval(ticker);
+    }, [text])//to run the effect whenever text changes
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2);
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Welcome to my Portfolio</span>
+                        <h1>{`Hi! I'm `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ " Kanishak Khanna","a Full Stack Developer", "a MERN Stack Developer", "a Machine Learning Engineer" ]'><span className="wrap">{text}</span></span></h1>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <button onClick={() => console.log('connect')}>Let's Connect <ArrowRightCircle size={25} /></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Header Img" />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
+export default Banner;
