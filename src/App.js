d0edbd20c40adf8ae03f3ef271635f765b20bf33
KAKA-14 <kanishakkhanna@gmail.com>
2023-10-13 23:25:46 +0530

    Projects

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from './components/NavBar';
 import Banner from './components/Banner';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
 function App() {
   return (
     <div>
       <NavBar/>
       <Banner/>
       <Skills/>
+      <Projects/>
     </div>
   );
 }
