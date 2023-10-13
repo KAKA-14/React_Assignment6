a3127041619ecf089860f019059c3a1487675387
KAKA-14 <kanishakkhanna@gmail.com>
2023-10-14 04:17:31 +0530

    fade in out removed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,8 +2,7 @@ import { useState, useEffect } from "react";
 import { Row, Container, Col } from "react-bootstrap";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import headerImg from '../assets/img/header-img.svg';
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+    
 const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);//to keep track of which word to display
     const [isDeleting, setIsDeleting] = useState(false);//to check if deleting is going on or not
@@ -42,16 +41,10 @@ const Banner = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <TrackVisibility>
-                            {({ isVisible }) =>
-                                <div className={isVisible ? "animate__animated animate__fadeInDown" : "animate__animated animate__fadeOutUp "}>
                                     <span className="tagline">Welcome to my Portfolio</span>
                                     <h1>{`Hi! I'm `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ " Kanishak Khanna","a Full Stack Developer", "a MERN Stack Developer", "a Machine Learning Engineer" ]'><span className="wrap">{text}</span></span></h1>
                                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                                     <button onClick={() => console.log('connect')}><a href="#connect">Let's Connect <ArrowRightCircle size={25} /></a></button>
-                                </div>
-                            }
-                        </TrackVisibility>
 
                     </Col>
                     <Col xs={12} md={6} xl={5}>
